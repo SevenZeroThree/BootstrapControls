d1d6e4ae9c85c934eabca3259d0dbfe0492cd5eb
sevenzerothree <ctobin@thesevenzerothree.com>
2014-04-02 14:50:05 -0400

    MVC Samples

---
+++ BootstrapControls.MVC.Samples/Controllers/ControlsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapControls.MVC.Samples.Controllers
+{
+    public class ControlsController : Controller
+    {
+        //
+        // GET: /Controls/
+        public ActionResult Button()
+        {
+            return View();
+        }
+
+        public ActionResult TextBox()
+        {
+            return View();
+        }
+	}
+}
